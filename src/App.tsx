65d2418e35548489b803d09a31b8fa54ceb71d6b
sharifabee <sharifabee.alur@gmail.com>
2024-12-19 15:17:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './comoponents/Home'
+import Movies from './comoponents/Movies'
+
+export default function App() {
+  return (
+    <div>
+
+      <Movies />
+    </div>
+  )
+}
